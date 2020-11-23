6272d27490581c19985aeef3bb7b9d640579a300
venkatesh <shariharanvenkatesh@gmail.com>
2020-11-23 20:14:10 +0530

    Added the yaml python dependency

--- app.py
+++ app.py
@@ -1,7 +1,26 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_mysqldb import MySQL
+import yaml
 
 app = Flask(__name__)
 
+
+# Configure db
+db = yaml.load(open('db.yaml'))
+app.config["MYSQL_HOST"] = db['mysql_host'] 
+app.config["MYSQL_USER"] = db['mysql_user']
+app.config["MYSQL_PASSWORD"] = db['mysql_password']
+app.config["MYSQL_DB"] = db['mysql_db']
+
+mysql = MySQL(app)
+
 @app.route('/')
 def index():
-    return "Hello world"
+    cur = mysql.connection.cursor()
+    data = cur.execute("SELECT * FROM tasks")
+    if data > 0:
+        users = cur.fetchall()
+        return render_template("index.html", users=users)
+    return 'No users'
+if __name__ == '__main__':
+    app.run(debug=True)
