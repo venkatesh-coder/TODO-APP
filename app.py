b31abe6d4531495cca3483ae1faa93926fdadb4e
venkatesh <shariharanvenkatesh@gmail.com>
2020-11-26 19:14:49 +0530

    added create and delete task option using sessions

--- app.py
+++ app.py
@@ -1,26 +1,46 @@
-from flask import Flask, render_template
-from flask_mysqldb import MySQL
-import yaml
+
+from flask import Flask , render_template, redirect, request, session
+from flask_session import Session
 
 app = Flask(__name__)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
 
+@app.route("/")
+def task():
+    if "todos" not in session:
+            session["todos"] = []
+    if len(session["todos"]) == 0:
+        return render_template("task.html", length = 0)
+    return render_template("task.html", todos=session["todos"])
 
-# Configure db
-db = yaml.load(open('db.yaml'))
-app.config["MYSQL_HOST"] = db['mysql_host'] 
-app.config["MYSQL_USER"] = db['mysql_user']
-app.config["MYSQL_PASSWORD"] = db['mysql_password']
-app.config["MYSQL_DB"] = db['mysql_db']
-
-mysql = MySQL(app)
-
-@app.route('/')
-def index():
-    cur = mysql.connection.cursor()
-    data = cur.execute("SELECT * FROM tasks")
-    if data > 0:
-        users = cur.fetchall()
-        return render_template("index.html", users=users)
-    return 'No users'
-if __name__ == '__main__':
-    app.run(debug=True)
+@app.route("/add", methods=["POST", "GET"])
+def add():
+    if request.method == "GET":
+        return render_template("add.html")
+    else:
+        todo = request.form.get("task")
+        session["todos"].append(todo)
+        return redirect("/")
+            
+            
+@app.route("/del", methods=["POST", "GET"])
+def delete():
+    if request.method == "GET":
+        return render_template("del.html", length = len(session["todos"]))
+    else:
+        num = request.form.get("dele")
+        if num.isdigit() == True:
+            num = int(num)
+            if (num > len(session["todos"])):
+                return render_template("invalidNum.html", num = num, length = len(session["todos"]))
+        elif num.isdigit() == False:
+            return render_template("noAlpha.html", length = len(session["todos"]))    
+        elif len(session["todos"]) == 0:
+            return redirect("task.html", length = 0)
+        session["todos"].pop(num-1)
+        return redirect("/")
+            
+        
+    
